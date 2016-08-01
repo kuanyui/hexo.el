a18dd5cb9f74b329b7053ef796197293beec4ab0
kuanyui <azazabc123@gmail.com>
2016-08-02 00:44:12 +0800

    Fix a extremely stupid bug

--- hexo.el
+++ hexo.el
@@ -707,7 +707,7 @@ truncated by `tabulated-list'."
   (interactive)
   (hexo-mode-only
    (let ((tag (ido-completing-read "Filter tag: "
-                                   (hexo-get-all-tags "~/source-kuanyui.github.io/") nil t)))
+                                   (hexo-get-all-tags (hexo-find-root-dir)) nil t)))
      (if (string= "" tag)
          (message "No tag inputed, abort.")
        ;; Assign variable `hexo-tabulated-list-entries-filter' as our filter function
