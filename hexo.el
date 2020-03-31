f6907f647cc8592124413ba2aee1a21efefc6732
cycoe <cycoe@cycoe.cc>
2020-03-31 22:28:47 +0800

    Delete the image directory when delete the article file
    For some people they store the images in the directory with same name of article
    file. So, we ask user to delete the image directory

--- hexo.el
+++ hexo.el
@@ -567,14 +567,16 @@ Please choose a POSIX-compatible shell.")
   (hexo-mode-only
    (hexo-mode-article-only
     (if (yes-or-no-p (format "Are you sure to delete file '%s' ?\nATTENTION: THIS ACTION CANNOT BE UNDONE!" (tabulated-list-get-id)))
-        (let ((filename (tabulated-list-get-id)))
+        (let* ((filename (tabulated-list-get-id))
+               (image-dir (car (split-string filename "\\."))))
           (delete-file filename)
-          (previous-line)
           (hexo-command-revert-tabulated-list)
-          (next-line)
-          (message (format "File \"%s\" deleted successfully." filename)))
-      (message "Action cancelled; nothing deleted.")
-  ))))
+          (if (yes-or-no-p (format "Do you want to also delete Directory \"%s\"?" image-dir))
+              (progn
+                (delete-directory image-dir t)
+                (message (format "Directory \"%s\" deleted successfully." image-dir)))
+            (message "Action cancelled; Directory is not deleted.")))
+      (message "Action cancelled; Nothing deleted.")))))
 
 (defun hexo-command-rename-file (&optional init-value)
   "Rename (mv) the filename of the article under the cursor."
